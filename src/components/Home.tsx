6c20c59e5da161e1b3513602bcb495073817b44c
HoangKhoi34 <65653624+Zenotorith@users.noreply.github.com>
2023-04-13 02:02:17 +0700

    add farmer-motion

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,25 @@
+import { motion } from 'framer-motion'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <main>
+      <div className='my-96 p-1'>
+        <h1 className='text-6xl text-center lg:text-right lg:textx-9xl'>Happy Pickle</h1>
+      </div>
+      <div className='flex justify-between'>
+        <div>
+          <h2>Design</h2>
+          <h2>Company</h2>
+          <h2>2023</h2>
+        </div>
+        <div>
+          <h3>This pickle goona make you smile.</h3>
+          <h3>Veinz designs to make you happy.</h3>
+          <h3>Click contact for cool transition.</h3>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default Home
