2bf34184852187bd4dbc60674a93b57370490c61
HoangKhoi34 <65653624+Zenotorith@users.noreply.github.com>
2023-05-03 08:24:25 +0700

    modify Testimonials padding

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -125,7 +125,7 @@ const About = () => {
           </div>
         </div>
       </div>
-      <div className='container px-5 py-24 mx-auto flex flex-col'>
+      <div className='container px-5 pb-24 mx-auto flex flex-col'>
         <div className='lg:w-4/6 mx-auto'>
           <div className='flex flex-col sm:flex-row mt-10'>
             <div className='sm:w-1/3 text-center sm:pr-8 sm:py-8'>
@@ -138,7 +138,7 @@ const About = () => {
                 <p className=' mt-1 text-gray-900 text-sm'>Thạc sĩ Khoa học tự nhiên</p>
                 <p className=' mt-1 text-gray-900 text-sm'>Thạc sĩ Quản trị kinh doanh</p>
                 <div className='w-12 h-1 bg-[#facd0e] rounded mt-2 mb-4'></div>
-                <p className='text-base'>Người giỏi tiếng Anh luôn sớm tìm ra trọng tâm và phương pháp học hiệu quả.</p>
+                <p className='text-base'>Từ quản lý tại tập đoàn đa quốc gia đến nhà giáo dục trẻ đầy tâm huyết.</p>
               </div>
             </div>
             <div className='sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left'>
