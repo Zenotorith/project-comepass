b9d60e3395f1b29451c0cd9eb54c2ef0f657273f
fxbite <letruongthinh1703@gmail.com>
2023-05-14 01:58:20 +0000

    Add description in html

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,6 @@ import logo from '../assets/logo.png'
 const Nav = () => {
   let Links = [
     { name: 'Trang Chủ', link: '/' },
-    { name: 'Khóa học', link: '/courses' },
     { name: 'Về Chúng Tôi', link: '/about-us' },
     { name: 'Liên Hệ', link: '/contact' }
   ]
